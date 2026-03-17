2c4b095d06cb65fdedfea443dd8f99563ef3cb9b
Joneus Lê <lelacbinh3107@gmail.com>
2026-03-17 23:17:55 +0700

    Coding 80%

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import Pager from "./assets/paper.png";
 import Scissors from "./assets/scissors.png";
 
 function App() {
+	const [isPlaying, setIsPlaying] = useState(false);
 	const [bot, setBot] = useState("Rock");
 	const [player, setPlayer] = useState("Rock");
+	const [countdown, setCountdown] = useState(null);
 
 	const options = ['Rock', 'Paper', "Scissors"];
 
@@ -27,24 +29,53 @@ function App() {
 
 	const gameStart = (choice = "") => {
 		if(!choice) return;
+		let count = 3;
 		const random = options[Math.floor(Math.random() * options.length)];
 		setPlayer(choice);
 		setBot(random);
+		setCountdown(count);
 
-		
+		const timer = setInterval(() => {
+			count -= 1;
+
+			if (count === 0) {
+				setCountdown(0);
+				clearInterval(timer);
+
+				// Small delay so "0" is visible before game starts
+				setTimeout(() => {
+					setCountdown(null);
+					setIsPlaying(true);
+				}, 600);
+
+			} else {
+				setCountdown(count);
+			}
+		}, 1000);
+
+		const checkResult = (player, bot) => {
+
+		}
 	}
 
 	return <>
+		{countdown !== null 
+			? <div className="modal">
+					<div className="modalOverlay"></div>
+					<span className="numberCountdown">{countdown}</span>
+				</div>
+			: null
+		}
 		<div className="playWrapper">
-			<div className="playArea">
+			<div className={`playArea ${isPlaying ? "is-active" : ""}`}>
 				<div className="bot choiceCard"><img src={getImage(bot)} alt="" /></div>
 				<div className="player choiceCard"><img src={getImage(player)} alt="" /></div>
 			</div>
 
 			<ul className="controller">
-				<li><button onClick={() => gameStart("Rock")}><img src={Rock} alt="rock" /></button></li>
-				<li><button onClick={() => gameStart("Paper")}><img src={Pager} alt="paper" /></button></li>
-				<li><button onClick={() => gameStart("Scissors")}><img src={Scissors} alt="scissors" /></button></li>
+				<li><button onClick={() => gameStart("Rock")} className={isPlaying&&'disabled'}><img src={Rock} alt="rock" /></button></li>
+				<li><button onClick={() => gameStart("Paper")} className={isPlaying&&'disabled'}><img src={Pager} alt="paper" /></button></li>
+				<li><button onClick={() => gameStart("Scissors")} className={isPlaying&&'disabled'}><img src={Scissors} alt="scissors" /></button></li>
 			</ul>
 		</div>
 	</>;
